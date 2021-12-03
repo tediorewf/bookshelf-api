f2b28fb3cbfcbee4c7ab86894c0fb6846b222db1
Mikhail Eremeev <meremeev@sfedu.ru>
2021-12-03 20:57:11 +0300

    feat: add api method to create token

--- app/controllers/api/v1/token_controller.rb
+++ app/controllers/api/v1/token_controller.rb
@@ -1,7 +1,22 @@
 module Api
   module V1
     class TokenController < ApplicationController
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+
       def create
+        user = User.find_by(email: params.require(:email))
+
+        if user&.authenticate(params.require(:password))
+          render json: { token: TokenHelper.encode(sub: user.id) }, status: :created
+        else
+          render json: { errors: user.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def parameter_missing(e)
+        render json: { error: e.message }, status: :unprocessable_entity
       end
     end
   end
--- app/helpers/token_helper.rb
+++ app/helpers/token_helper.rb
@@ -1,16 +1,14 @@
-class TokenService
+module TokenHelper
+  HMAC_SECRET = Rails.application.credentials.fetch(:secret_key_base)
+  private_constant :HMAC_SECRET
+
   class << self
     def encode(payload)
-      lifetime = Rails.application.config.x.token.lifetime
-      expiration = lifetime.from_now.to_i
-      payload.merge!(exp: expiration)
-      hmac_secret = Rails.application.credentials.fetch(:secret_key_base)
-      JWT.encode(payload, hmac_secret)
+      JWT.encode(payload, HMAC_SECRET)
     end
 
     def decode(token)
-      hmac_secret = Rails.application.credentials.fetch(:secret_key_base)
-      JWT.decode(token, hmac_secret).first
+      JWT.decode(token, HMAC_SECRET).first
     end
   end
 end
