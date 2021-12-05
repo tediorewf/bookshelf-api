432c08bdbea3d2f4cb65f69539a1191899f9fafc
Mikhail Eremeev <meremeev@sfedu.ru>
2021-12-05 12:14:59 +0300

    test: add basic tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without email and password" do
+    user = User.new
+    assert_not user.save, "Saved the user without the email and password"
+  end
+
+  test "should normalize email before save" do
+    user = User.new(email: "TeSt@TeSt.TeSt", password: "Password123")
+    user.save
+    assert_equal "test@test.test", user.email, "Saved the user with unnormalized email"
+  end
 end
