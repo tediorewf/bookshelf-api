212162dc46e007a01a1335cff4604f257f5f9ea8
Mikhail Eremeev <meremeev@sfedu.ru>
2021-12-07 23:49:20 +0300

    test: add tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,38 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   test "should not save user without email and password" do
     user = User.new
-    assert_not user.save, "Saved the user without the email and password"
+    saved = user.save
+
+    assert_not saved, "Saved the user without the email and password"
+  end
+
+  test "should not save user without password confirmation" do
+    email = "test@test.test"
+    password = "password"
+    attributes = {
+      email: email,
+      password: password
+    }
+
+    user = User.new(attributes)
+    saved = user.save
+
+    assert_not saved, "Saved the user without password confirmation"
   end
 
   test "should normalize email before save" do
-    user = User.new(email: "TeSt@TeSt.TeSt", password: "Password123")
-    user.save
+    email = "TeSt@TeSt.TeSt"
+    password = "password"
+    password_confirmation = "password"
+    attributes = {
+      email: email,
+      password: password,
+      password_confirmation: password_confirmation
+    }
+
+    user = User.new(attributes)
+    _ = user.save
+
     assert_equal "test@test.test", user.email, "Saved the user with unnormalized email"
   end
 end
