5930d5fdaa6545ee6350d1d5a63d1cfd6b13dbd3
Mikhail Eremeev <meremeev@sfedu.ru>
2021-12-07 23:58:11 +0300

    test: add tests for token controller

--- test/controllers/api/v1/token_controller_test.rb
+++ test/controllers/api/v1/token_controller_test.rb
@@ -1,4 +1,34 @@
 require "test_helper"
 
 class Api::V1::TokenControllerTest < ActionDispatch::IntegrationTest
+  TOKEN_PROPERTY_NAME = 'token'
+  private_constant :TOKEN_PROPERTY_NAME
+
+  test "should create token for existing user" do
+    email = user_one_email
+    password = user_one_password
+    params = {
+      email: email,
+      password: password
+    }
+
+    post api_v1_token_url params: params
+
+    assert_response :created
+    assert_not_nil response.body[TOKEN_PROPERTY_NAME]
+  end
+
+  test "should not create token for not existing user" do
+    email = "not@existing.email"
+    password = "not-existing-password"
+    params = {
+      email: email,
+      password: password
+    }
+
+    post api_v1_token_url params: params
+
+    assert_response :bad_request
+    assert_nil response.body[TOKEN_PROPERTY_NAME]
+  end
 end
