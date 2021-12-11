d7b218d4cddb4682066537a26cfdbea06789bf4a
Mikhail Eremeev <meremeev@sfedu.ru>
2021-12-11 15:31:34 +0300

    refactor(tests): remove unnecessary test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
-require "test_helpers/book_fixtures_attributes"
 require "test_helpers/password_digest_helper"
-require "test_helpers/user_fixtures_attributes"
 require "test_helpers/token_helper"
-require "test_helpers/reader_fixture_attributes"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -16,11 +13,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  include UserFixturesAttributes
   include TokenHelper
 end
 
 ActiveRecord::FixtureSet.context_class.include PasswordDigestHelper
-ActiveRecord::FixtureSet.context_class.include UserFixturesAttributes
-ActiveRecord::FixtureSet.context_class.include BookFixtureAttributes
-ActiveRecord::FixtureSet.context_class.include ReaderFixtureAttributes
