719fdded6ccb9c67ca6e483276bd9ecfe35a389f
Mikhail Eremeev <meremeev@sfedu.ru>
2021-12-17 20:02:13 +0300

    test: add borrowing model basic test
    Add test to check that Borrowing model should be valid

--- test/models/borrowing_test.rb
+++ test/models/borrowing_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class BorrowingTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  setup do
+    @user = users(:two)
+    @reader = readers(:two)
+    @book = books(:two)
+
+    @borrowing  = Borrowing.new(user: @user,
+                                reader: @reader,
+                                book: @book)
+  end
+
+  test 'borrowing should be valid' do
+    assert @borrowing.valid?
   end
 end
